ef67315e61d670c303dcad16fbfb82a829f088b4
Princewill Iroka <princewilliroka@outlook.com>
2023-11-13 21:12:10 +0100

    Added browse section to Homepage

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,5 +1,13 @@
 import React from "react";
 import { AiOutlineArrowRight } from "react-icons/ai";
+import { BsFileMusic } from "react-icons/bs";
+import {
+  MdOutlineSportsSoccer,
+  MdTheaterComedy,
+  MdOutlineFestival,
+} from "react-icons/md";
+import { FaArtstation } from "react-icons/fa";
+import { GrTechnology } from "react-icons/gr";
 import "./Home.css";
 
 function Home() {
@@ -23,7 +31,7 @@ function Home() {
         </ul>
       </header>
       <section>
-        <div className="banner-container">
+        <div className="banner-section">
           <div className="banner-content">
             <h1 className="banner-title">
               Buy or Sell your event tickets easily wherever you are
@@ -34,6 +42,44 @@ function Home() {
             <button className="btn-cta">
               Find an event <AiOutlineArrowRight className="arrow-right" />
             </button>
+          </div>
+        </div>
+        <div className="browse-section">
+          <div className="browse-category">
+            <div className="browse-content">
+              <BsFileMusic className="browse-icon" />
+            </div>
+            <span className="browse-item">Music</span>
+          </div>
+          <div className="browse-category">
+            <div className="browse-content">
+              <MdOutlineSportsSoccer className="browse-icon" />
+            </div>
+            <span className="browse-item">Sports</span>
+          </div>
+          <div className="browse-category">
+            <div className="browse-content">
+              <FaArtstation className="browse-icon" />
+            </div>
+            <span className="browse-item">Arts & Theater</span>
+          </div>
+          <div className="browse-category">
+            <div className="browse-content">
+              <MdTheaterComedy className="browse-icon" />
+            </div>
+            <span className="browse-item">Comedy</span>
+          </div>
+          <div className="browse-category">
+            <div className="browse-content">
+              <MdOutlineFestival className="browse-icon" />
+            </div>
+            <span className="browse-item">Festivals</span>
+          </div>
+          <div className="browse-category">
+            <div className="browse-content">
+              <GrTechnology className="browse-icon" />
+            </div>
+            <span className="browse-item">Technology</span>
           </div>
         </div>
       </section>
