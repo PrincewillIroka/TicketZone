1510c3099a0983b176463150b1b28a16a873b537
Princewill Iroka <princewilliroka@outlook.com>
2023-12-31 16:17:58 +0100

    Added Auth route

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import Auth from "./components/Auth";
 
 function App() {
   return (
     <div>
-      <Home />
+      <Router>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/auth" element={<Auth />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
