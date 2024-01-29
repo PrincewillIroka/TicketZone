aa5c16399ae535cbb217c8bb1db9b74783876b0f
Princewill Iroka <princewilliroka@outlook.com>
2024-01-29 11:43:31 +0100

    Added userLogin functionality

--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import "./Auth.css";
+import { userLogin } from "../../services/authServices";
 
 const BTN_CAPTION = {
   login: `Don't have an account ? `,
@@ -12,10 +13,13 @@ const BTN_TAG = {
   signup: `Login`,
 };
 
-function Auth(props) {
+function Auth() {
   const navigate = useNavigate();
   const location = useLocation();
   const activeAction = location.pathname.split("/")[1];
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isLoading, setIsLoading] = useState("");
 
   const getActiveTab = (value) => location.pathname.includes(value);
 
@@ -27,26 +31,51 @@ function Auth(props) {
     }
   };
 
+  const handleLogin = async () => {
+    setIsLoading(true);
+    await userLogin({ email, password }).then((response) => {
+      const { success, data } = response;
+      if (success) {
+        navigate("/dashboard", { replace: true });
+      }
+      setIsLoading(false);
+    });
+  };
+
+  const handleSignUp = () => {};
+
   return (
     <section className="auth-container">
       <div className="auth-content">
         <form className="auth-form">
           <h1 className="auth-title">Ticket Zone</h1>
-          <input placeholder="Email" className="auth-input" />
+          <input
+            placeholder="Email"
+            className="auth-input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
           <input
             placeholder="Password"
             type="password"
             className="auth-input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
           />
-          <button className="btn-auth-submit">
+          <button
+            className="btn-auth-submit"
+            onClick={(e) => {
+              getActiveTab("login") ? handleLogin() : handleSignUp();
+            }}
+          >
             {getActiveTab("login") ? "Login" : "Sign Up"}
           </button>
         </form>
         <p>
           {BTN_CAPTION[activeAction]}
-          <a className="signup-link" onClick={toggleActiveAction}>
+          <span className="signup-link" onClick={toggleActiveAction}>
             {BTN_TAG[activeAction]}
-          </a>
+          </span>
         </p>
       </div>
       <div className="auth-bg" />
---
+++ src/services/authServices.js
@@ -0,0 +1,23 @@
+const APP_BACKEND = process.env.REACT_APP_BACKEND;
+
+const userLogin = ({ email, password }) => {
+  return new Promise((resolve, reject) => {
+    const url = `${APP_BACKEND}/api/login`;
+
+    fetch(url, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    })
+      .then((response) => response.json())
+      .then((response) => resolve(response))
+      .catch((error) => {
+        console.error(error);
+        return reject(error);
+      });
+  });
+};
+
+export { userLogin };
