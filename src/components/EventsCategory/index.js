1f274788ba2f66d249a79cddfb44b4b6f9deb4f0
Princewill Iroka <princewilliroka@outlook.com>
2024-05-18 21:04:07 +0100

    Update EventsCategory page

--- src/components/EventsCategory/index.js
+++ src/components/EventsCategory/index.js
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useLocation } from "react-router-dom";
 import Header from "../Header";
 import Footer from "../Footer";
+import "./EventsCategory.css";
+import { getEventsCategory } from "services/eventServices";
 
-function Events(props) {
+function EventsCategory() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [eventsCategory, setEventsCategory] = useState([]);
+  const { pathname } = useLocation();
+  const alias = pathname.split("/")[2];
+
+  useEffect(() => {
+    handleGetEventsCategory();
+  }, []);
+
+  const handleGetEventsCategory = () => {
+    getEventsCategory().then((response) => {
+      const { success, data } = response || {};
+      console.log({ data });
+      if (success) {
+        // setEvents(data);
+      }
+    });
+  };
+
   return (
     <div>
       <Header />
+      <div className="events-category-container">
+        {isLoading ? <div className="">Loading...</div> : <div>Events</div>}
 
-      {/* <Footer /> */}
+        {/* <Footer /> */}
+      </div>
     </div>
   );
 }
 
-export default Events;
+export default EventsCategory;
