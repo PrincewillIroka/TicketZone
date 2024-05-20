2e59f26866da0a50fdefeaab6ec18e7abca424b8
Princewill Iroka <princewilliroka@outlook.com>
2024-05-20 21:10:57 +0100

    Update pages

--- src/components/BuyATicket/index.js
+++ src/components/BuyATicket/index.js
@@ -1,11 +1,16 @@
 import React from "react";
 import Header from "../Header";
+import Footer from "../Footer";
+import "./BuyATicket.css";
 
 function BuyATicket(props) {
   return (
     <div>
       <Header />
-      BuyATicket
+      <div className="layout-container">
+        <section className="buy-a-ticket-section"></section>
+      </div>
+      <Footer />
     </div>
   );
 }
