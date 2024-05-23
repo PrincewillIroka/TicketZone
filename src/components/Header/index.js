cb7eae94addcc21164dcb83516995ef8e779bc8c
Princewill Iroka <princewilliroka@outlook.com>
2024-05-23 03:37:26 +0100

    Updated Cart UI display

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLocation, useNavigate } from "react-router-dom";
 import { FaCircleUser } from "react-icons/fa6";
 import { FiShoppingCart } from "react-icons/fi";
+import { IoMdClose } from "react-icons/io";
 import { useStateValue } from "store/stateProvider";
 import "./Header.css";
 
@@ -10,6 +11,7 @@ function Header() {
   const navigate = useNavigate();
   const location = useLocation();
   const { isUserLoggedIn, ticketCart = [] } = state;
+  const [isCartOpen, setIsCartOpen] = useState(false);
 
   const handleNavigate = (page) => {
     navigate(page);
@@ -64,7 +66,10 @@ function Header() {
                 title="Dashboard"
                 onClick={() => handleNavigate("/dashboard")}
               />
-              <div className="cart-icon-wrapper">
+              <div
+                className="cart-icon-wrapper"
+                onClick={() => setIsCartOpen(true)}
+              >
                 <FiShoppingCart className="cart-icon" />
                 <span className="cart-count">{ticketCart.length}</span>
               </div>
@@ -72,6 +77,40 @@ function Header() {
           )}
         </li>
       </ul>
+      {isCartOpen && (
+        <div className="cart-container">
+          <div className="cart-content">
+            <div className="cart-header">
+              <h3 className="cart-heading">Cart</h3>
+              <IoMdClose
+                className="cart-close-btn"
+                onClick={() => setIsCartOpen(false)}
+              />
+            </div>
+            <div className="cart-body">
+              {ticketCart.map((ticketItem, index) => {
+                const { ticket, ticketQuantity } = ticketItem;
+                return (
+                  <div key={index} className="cart-ticket">
+                    <span>{ticket.title}</span>
+                    <span>{ticketQuantity}</span>
+                  </div>
+                );
+              })}
+            </div>
+            <div className="cart-footer">
+              <div className="cart-items-summary">
+                <span>Items</span>
+                <span>0.00</span>
+              </div>
+              <div className="cart-items-summary">
+                <span>Total</span>
+                <span>0.00</span>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
     </header>
   );
 }
