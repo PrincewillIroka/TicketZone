d8862538090188ad38db9a74e8c0a2f7c9a1fb9c
Princewill Iroka <princewilliroka@outlook.com>
2024-06-19 17:37:50 +0100

    Update loading event tags

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -12,22 +12,23 @@ import { FaArtstation, FaBible, FaMoneyCheckAlt } from "react-icons/fa";
 import { GrTechnology } from "react-icons/gr";
 import CurveSvg from "assets/Curve.svg";
 import { getCategories, getEvents } from "services/eventServices";
-import { createArrayItems } from "utils";
+import { createArrayItems, DEFAULT_EVENT_TAGS } from "utils";
 import { useStateValue } from "store/stateProvider";
 import "./Home.css";
 import Header from "components/Header";
 import Footer from "components/Footer";
 import EventCard from "components/EventCard";
 
-const defaultTag = { name: "All", label: "all" };
-
 function Home() {
-  const [activeTab, setActiveTab] = useState(defaultTag.label);
-  const [isLoading, setIsLoading] = useState(true);
   const navigate = useNavigate();
   const { state = {}, dispatch } = useStateValue();
   const { homePage = {} } = state;
   let { categories = [], displayedTags = [], events = [] } = homePage;
+  const [activeTab, setActiveTab] = useState(DEFAULT_EVENT_TAGS["label"]);
+  const [isLoadingEvents, setIsLoadingEvents] = useState(true);
+  const [isLoadingCategories, setIsLoadingCategories] = useState(
+    categories.length ? false : true
+  );
 
   const handleSetActiveTab = (activeTab) => {
     setActiveTab(activeTab);
@@ -40,13 +41,12 @@ function Home() {
   const handleGetCategories = useCallback(() => {
     getCategories().then((response) => {
       const { success, categories = [], tags = [] } = response || {};
-      console.log({ categories });
       if (success) {
         dispatch({
           type: "UPDATE_HOME_PAGE_CATEGORIES",
           payload: { categories, displayedTags: tags },
         });
-        setIsLoading(false);
+        setIsLoadingCategories(false);
       }
     });
   }, [dispatch]);
@@ -59,14 +59,17 @@ function Home() {
           type: "UPDATE_HOME_PAGE_EVENTS",
           payload: { events: data, eventsClone: data },
         });
+        setIsLoadingEvents(false);
       }
     });
   }, [dispatch]);
 
   useEffect(() => {
-    handleGetCategories();
+    if (!categories.length) {
+      handleGetCategories();
+    }
     handleGetEvents();
-  }, [handleGetCategories, handleGetEvents]);
+  }, [handleGetCategories, handleGetEvents, categories]);
 
   const getCategoryIcon = (name) => {
     let icon,
@@ -115,8 +118,8 @@ function Home() {
           </div>
         </section>
         <section className="browse-section">
-          {isLoading
-            ? createArrayItems(6).map((item, index) => (
+          {isLoadingCategories
+            ? createArrayItems(7).map((item, index) => (
                 <div className="browse-category" key={index}>
                   <div className="browse-content"></div>
                 </div>
@@ -155,7 +158,7 @@ function Home() {
             ))}
           </ul>
           <div className="explore-items">
-            {isLoading ? (
+            {isLoadingEvents ? (
               createArrayItems(8).map((item, index) => (
                 <div
                   className="explore-item-wrapper shimmer-bg"
